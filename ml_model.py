c6b11dbf4419e4d2fbb32493a99bc7c5b07554f3
Haniiska <haniiska108@gmail.com>
2026-03-09 16:02:38 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,16 @@
+from sklearn.ensemble import IsolationForest
 
+def detect_anomalies(df):
+
+    model = IsolationForest(contamination=0.1, random_state=42)
+
+    features = df[["Billed_Amount_USD","Actual_Payment_USD"]]
+
+    df["AI_Anomaly"] = model.fit_predict(features)
+
+    df["AI_Anomaly"] = df["AI_Anomaly"].map({
+        1:"Normal",
+        -1:"Suspicious"
+    })
+
+    return df
